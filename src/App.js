83ecb983c95bd058bfc4ee4c635280de0e3ef7cb
Thang La <thangla@Thangs-MacBook-Pro.local>
2019-08-27 11:44:53 +0700

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './assets/styles/responsive.css';
+import {Switch,Route} from 'react-router-dom';
+import Header from './components/include/header';
 
 class App extends Component {
   render() {
     return(
       <React.Fragment>
-
+        <Header />
       </React.Fragment>
     );
   }
